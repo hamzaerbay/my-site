7423e428abf45e43e24409bc304bd0522582f4b9
Hamza Erbay <hamza@udemy.com>
2019-03-25 22:14:31 -0700

    layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,8 +9,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
 
-// import './layout.css';
-import './old.css';
+import './layout.css';
 
 const Layout = ({ children }) => (
   <StaticQuery
